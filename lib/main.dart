bc1b94f87b40def54d0eacccaf158f5176db213d
albemala <albemala@gmail.com>
2023-06-29 10:10:52 +0200

    added font files

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:dart_lens/blocs/project-analysis-bloc.dart';
 import 'package:dart_lens/views/app-view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
+
   runApp(
     MultiBlocProvider(
       providers: [
