562cef72383063cf60661cd6945443113cd068b1
albemala <albemala@gmail.com>
2023-09-20 20:43:50 +0200

    setup build app for macos

---
+++ scripts/build-macos.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Usage: bash scripts/build-macos.bash
+
+set -e # exit on error
+#set -x # print commands
+
+APP_VERSION=$(sed -n 's/^version: //p' pubspec.yaml)
+
+# clean
+flutter clean
+
+# prepare build
+flutter build macos --release
+cd macos
+fastlane mac get_certificates_profiles
+
+# build standalone
+rm -rf build
+fastlane mac build_standalone
+# publish standalone
+fastlane mac publish_standalone
+# archive standalone
+cd ..
+ARCHIVE_PATH=macos-builds/$APP_VERSION
+rm -rf $ARCHIVE_PATH
+mkdir -p $ARCHIVE_PATH
+cp -r "macos/build/DartLens.app.zip" $ARCHIVE_PATH
+cp -r "macos/build/DartLens.app.dSYM.zip" $ARCHIVE_PATH
---
+++ scripts/generate-apple-certificates-profiles.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Usage: bash scripts/generate-apple-certificates-profiles.bash
+
+set -e # exit on error
+#set -x # print commands
+
+cd macos
+fastlane mac generate_certificates_profiles
